62d00983d1dbd113c1fd43a865a7f7a575049905
ananaw <61036678+ananaw@users.noreply.github.com>
2020-02-29 16:33:27 -0800

    Update my_switch.c
    Complete code with comments. Good for lab report

--- my_switch/my_switch.c
+++ my_switch/my_switch.c
@@ -1,13 +1,13 @@
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
 
-#define 	BLUE_MASK 		0x04
+#define 	RED_MASK 		0x02
 //*****************************************************************************
 //
 //!
-//! A very simple example that interfaces with the blud LED (PF2) and SW2 (PF0) 
-//! using direct register access. When SW2 is pressed, the LED is turned on. When 
-//! SW2 is released, the LED is turned off. 
+//! A very simple example that interfaces with the red LED (PF1) and SW2 (PF0) 
+//! using direct register access. When SW2 is pressed, the LED is turned off. When 
+//! SW2 is released, the LED is turned on. 
 //
 //*****************************************************************************
 
@@ -16,35 +16,36 @@ void
 PortFunctionInit(void)
 {
 
-		volatile uint32_t ui32Loop;   // value could change anytime
-	
+		volatile uint32_t ui32Loop;   			// makes sure compiler checks for difference, 
+																				// volatile ensures compiler always carries out memory accesses.
 	// Enable the clock of the GPIO port that is used for the on-board LED and switch.
     SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
 
     //
     // Do a dummy read to insert a few cycles after enabling the peripheral.
     //
-    ui32Loop = SYSCTL_RCGC2_R;		// 32 bit unsigned integer
+    ui32Loop = SYSCTL_RCGC2_R;					// 32 bit unsigned integer
 
     // Unlock GPIO Port F
     GPIO_PORTF_LOCK_R = 0x4C4F434B;   	// unlock port F, address is the key
     GPIO_PORTF_CR_R |= 0x01;           	// allow changes to PF0, bitwise OR assignment
 
-    GPIO_PORTF_AMSEL_R &= ~0x00;		// clear AMSEL (analog mode selection)
-    GPIO_PORTF_PCTL_R &= ~0x00000000;	// PCTL GPIO on PF7-0, select GPIO, clear all
-    // Set direction of PF1(0010) as output (PF1-red LED)		// Set the direction of PF2 (x04 blue LED) as output 		
-    GPIO_PORTF_DIR_R |= 0x02;		// DIR_R specifies input/output of port with 0/1 respectively. 
+    GPIO_PORTF_AMSEL_R &= ~0x00;				// clear AMSEL (disable analog mode selection)
+    GPIO_PORTF_PCTL_R &= ~0x00000000;		// PCTL GPIO on PF7-0, select GPIO, clear all
+	
+    // Set direction of PF1(0010) as output (PF1-red LED). Set the direction of PF2 (x04 blue LED) as output 		
+    GPIO_PORTF_DIR_R |= 0x02;						// DIR_R specifies input/output of port with 0/1 respectively. 
 	
     // Set the direction of PF0 (SW2) as input by clearing the bit
-    GPIO_PORTF_DIR_R &= ~0x01;		// bitwise AND assignment to complement of 01
+    GPIO_PORTF_DIR_R &= ~0x01;					// bitwise AND assignment to complement of 01
 
-    GPIO_PORTF_AFSEL_R = 0x00;
+    GPIO_PORTF_AFSEL_R &= ~0x00; 				// clear AFSEL bits to 0 to select regular I/O
 	
-    // Enable only(for PF1 & PF0, use x03)		// Enable both PF2 and PF0 for digital function.(05) 
-    GPIO_PORTF_DEN_R |= 0x03;
+    // Enable only(for PF1 & PF0, use x03). To enable both PF2 and PF0 for digital function.(05) 
+    GPIO_PORTF_DEN_R |= 0x03;						// DEN_R enables for digital function
 	
-		//Enable pull-up on PF0
-		GPIO_PORTF_PUR_R |= 0x01; 
+    //Enable internal pull-up on PF0
+    GPIO_PORTF_PUR_R |= 0x01; 					
 
 }
 
@@ -55,7 +56,6 @@ int main(void)
 		//initialize the GPIO ports	
 		PortFunctionInit();
 	
-	
     //
     // Loop forever.
     //
@@ -64,13 +64,13 @@ int main(void)
 
         if((GPIO_PORTF_DATA_R&0x01)==0x00) //SW2 is pressed
 				{
-						// Turn off the LED.
-						GPIO_PORTF_DATA_R &= ~0x02; 	// 0x04 for blue LED, 02 for RED, ~ is 
-				}						// bitwise complement, & = bitwise AND
+						// Turn off the LED. Clear bits in PF1
+						GPIO_PORTF_DATA_R &= ~0x02; 	 // 0x04 for blue LED, 02 for RED, ~ is 
+				}																	 // bitwise complement, & = bitwise AND
 				else
 				{
-						// Turn on the LED.
-						GPIO_PORTF_DATA_R |= 0x02;	// 0x04 for blue LED, 02 for RED
-				}						// | = bitwise OR
+						// Turn on the LED. Set 1 in PF1
+						GPIO_PORTF_DATA_R |= 0x02;		 // 0x04 for blue LED, 02 for RED
+				}																	 // | = bitwise OR
     }
 }
